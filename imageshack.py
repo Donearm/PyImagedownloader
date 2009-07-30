3bc8b4947817e1352dcd7c7432a779d514476817
Gianluca Fiore <forod.g@gmail.com>
2009-07-30 14:24:58 +0200

    imageshack code updated and working again

--- imageshack.py
+++ imageshack.py
@@ -32,7 +32,7 @@ headers = { 'User-Agent' : user_agent }
 data = urlencode(values)
 
 def imageshack_parse(link):
-    #rSrcImageshack = re.compile('http://www\.yfrog\.com/\?url=http://img([0-9]{,3})\.imageshack\.us/img[0-9]+/[0-9]+/[0-9a-z]+\.[jpg|gif|png]')
+    rSrcImageshack = re.compile('/img[0-9]+/[0-9]+/[a-zA-Z0-9]+\.[jpg|gif|png]', re.IGNORECASE)
     imageshack_list = [] # the list that will contain the href tags
     imageshack_list.append(link['href'])
     for i in imageshack_list:
@@ -47,18 +47,20 @@ def imageshack_parse(link):
         image_page = response.read()
         page_soup = BeautifulSoup(image_page)
         # find the src attribute which contains the real link of imageshack's images
-        #src_links = page_soup.findAll('a', href=rSrcImageshack)
-        src_links = page_soup.findAll('link', rel='image_src')
+        src_links = page_soup.findAll('img', src=rSrcImageshack)
+        #src_links = page_soup.findAll('link', rel='image_src')
         imageshack_src = []
         for li in src_links:
-            imageshack_src.append(li['href']) # add all the src part to a list
+            imageshack_src.append(li['src']) # add all the src part to a list
 
         try:
             # generate just the filename of the image to be locally saved
             save_extension = re.split('img[0-9]{,3}/[0-9]+/', imageshack_src[0])
+            # extract just the first part of the url to join with imageshack_src
+            url_extension = re.split('/i/', i)
 
             savefile = basedir + str(save_extension[1])
-            download_url = imageshack_src[0]
+            download_url = url_extension[0] + imageshack_src[0]
             # finally save the image on the desidered directory
             urlretrieve(download_url, savefile) 
         except IndexError:
