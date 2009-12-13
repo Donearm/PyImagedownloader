579d7b8baafdfde000d02d4827bdbf196c2a6aca
Gianluca Fiore <forod.g@gmail.com>
2009-12-13 11:59:12 +0100

    updated __init__.py to the new modules

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-__all__ = ["imagevenue", "imagebam", "imageshack", "imagehaven", "imgshed", "uppix", "usercash", "bellazon", "skinsbe", "savesource"]
+__all__ = ["imagevenue", "imagebam", "imageshack", "imagehaven", "imgshed", "uppix", "bellazon", "skinsbe", "savesource", "imagetitan", "shareapic", "storeimgs", "upmyphoto", "sharenxs", "blogspot"]
