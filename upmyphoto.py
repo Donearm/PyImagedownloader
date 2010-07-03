e36a6708a70b471d8496e68c26e1e3e33d7b0039
Gianluca Fiore <forod.g@gmail.com>
2010-07-03 08:15:10 +0200

    corrected upmyphoto's save_extension

--- upmyphoto.py
+++ upmyphoto.py
@@ -57,8 +57,8 @@ def upmyphoto_parse(link):
         upmyphoto_src = [li.get('src', None) for li in src_links]
 
         # generate just the filename of the image to be locally saved
-        save_extension = re.split('/img/dir[0-9]+', upmyphoto_src[0])
-        savefile = basedir + str(save_extension[1])
+        save_extension = re.split('/img/dir[0-9]+/(loc[0-9]+/)?', upmyphoto_src[0])
+        savefile = basedir + str(save_extension[-1])
 
         download_url = upmyphoto_src[0]
         # finally save the image on the desidered directory
