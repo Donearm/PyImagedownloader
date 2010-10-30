e927b4b887dc3fb4f966b22375a828e88971fe9d
Gianluca Fiore <forod.g@gmail.com>
2010-10-30 12:03:20 +0200

    added example of config file

---
+++ pyimg-example.py
@@ -0,0 +1,23 @@
+# The directory where to download the images. Full path
+basedir = ''
+
+# The User Agent string for the connection. Default is for Firefox 4.0b6 on Linux x86_64, change it if you like
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0b6pre) Gecko/20100910 Firefox/4.0b6pre'
+
+# Timeout after when the connection is dropped (in secs). Default 120
+timeout = 120
+
+# Debug or not?
+debug = 0
+
+# Celebrityforum username and password
+cf_name = ''
+cf_pwd = ''
+
+# IMC website username and password
+imc_name = ''
+imc_pwd = ''
+
+# Usemycomputer forum username and password
+umc_name = ''
+umc_pwd = ''
