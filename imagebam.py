a5fc40bbcc2f9f82682453986fd041b7d7ae685c
Gianluca Fiore <forod.g@gmail.com>
2010-12-19 18:44:09 +0100

    ignoring parsing errors in imagebam.py

--- imagebam.py
+++ imagebam.py
@@ -28,7 +28,11 @@ def imagebam_parse(link, basedir):
     connector = http_connector.Connector()
     response = connector.reqhandler(link)
 
-    page = lxml.html.fromstring(response)
+    try:
+        page = lxml.html.fromstring(response)
+    except lxml.etree.XMLSyntaxError as e:
+        # most of the time we can simply ignore parsing errors
+        pass
 
     # find the src attribute which contains the real link of imagebam's images
     src_links = page.xpath("//img[@onclick='scale(this);']")
