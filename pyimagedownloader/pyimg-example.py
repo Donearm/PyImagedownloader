f4a102782bfd9ddf8224954df2d73062fa36ed58
Gianluca Fiore <forod.g@gmail.com>
2011-03-18 15:03:06 +0100

    updated user-agent string

--- pyimagedownloader/pyimg-example.py
+++ pyimagedownloader/pyimg-example.py
@@ -1,8 +1,8 @@
 # The directory where to download the images. Full path
 basedir = ''
 
-# The User Agent string for the connection. Default is for Firefox 4.0b12 on Linux x86_64, change it if you like
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0b12) Gecko/20100101 Firefox/4.0b12'
+# The User Agent string for the connection. Default is for Firefox 4.0 on Linux x86_64, change it if you like
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64; rv:2.0) Gecko/20100101 Firefox/4.0'
 
 # Timeout after when the connection is dropped (in secs). Default 120
 timeout = 120
