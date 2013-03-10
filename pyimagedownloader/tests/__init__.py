2058207d2fcd150ed8e8b16c3399d61c94afb44f
Gianluca Fiore <forod.g@gmail.com>
2013-03-10 14:40:56 +0100

    add all tests to __init__.py

--- pyimagedownloader/tests/__init__.py
+++ pyimagedownloader/tests/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ['bellazon_test', 'http_connector_test', 'pyimagedownloader_test', 'savesource_test']
+__all__ = ['bellazon_test', 'http_connector_test', 'imagebam_test', 
+'imagehaven_test', 'imagehostorg_test', 'imageshack_test', 'imagetitan_test', 
+'imageupper_test', 'imgbox_test', 'imgchili_test', 'imgur_test', 
+'pixroute_test', 'postimage_test', 'pyimagedownloader_test', 'radikal_test', 
+'savesource_test', 'servimg_test', 'shareapic_test', 'sharenxs_test', 
+'skinsbe_test', 'tumblr_test', 'turboimagehost_test', 'typepad_test', 
+'usemycomputer_test']
